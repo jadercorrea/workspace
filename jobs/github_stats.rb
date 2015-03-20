a5cef60763a3938dfc1ad398ec49c64025b9b474
Jader Correa <jadercorrea.hv@gmail.com>
2015-03-19 22:06:26 -0300

    Add github widget

---
+++ jobs/github_stats.rb
@@ -0,0 +1,29 @@
+require 'octokit'
+require 'action_view'
+include ActionView::Helpers::DateHelper
+
+config = YAML::load_file('config.yml')
+
+Octokit.configure do |c|
+  c.auto_paginate = true
+  c.login = config["login"]
+  c.password = config["password"]
+end
+
+
+SCHEDULER.every '2m', :first_in => 0 do |job|
+  config["repos"].each do |name|
+    r = Octokit::Client.new.repository(name)
+    pulls = Octokit.pulls(name, :state => 'open').count
+
+    send_event('github', {
+      repo: name.slice(/([^\/]+)$/),
+      issues: r.open_issues_count - pulls,
+      pulls: pulls,
+      forks: r.forks,
+      watchers: r.subscribers_count,
+      stargazers: r.stargazers_count,
+      activity: time_ago_in_words(r.updated_at).capitalize
+    })
+  end
+end
