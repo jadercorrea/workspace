f7fd8e176822e9bde24966a0510ddefbfd7a4438
Jader Correa <jadercorrea.hv@gmail.com>
2015-03-26 11:43:24 -0300

    Remove sample

--- jobs/sample.rb
+++
@@ -1,9 +0,0 @@
-current_karma = 0
-
-SCHEDULER.every '2s' do
-  last_karma     = current_karma
-  current_karma     = rand(200000)
-
-  send_event('karma', { current: current_karma, last: last_karma })
-  send_event('synergy',   { value: rand(100) })
-end
